b2e458d218cab7d133a0cad16084d0135964ee29
Duwenjing <898607505@qq.com>
2020-02-04 16:53:49 +0800

    111

---
+++ demo.py
@@ -0,0 +1,16 @@
+# value = input("请输入你需要的值：")
+# print(type(value))
+# print("输入的值是：",value)
+
+
+#num = len("哈哈哈哈哈哈哈哈哈哈哈好")
+#print(num)
+
+#获取输入值，输出你输入的长度
+
+#value = input("请输入")
+#num = len(value)
+#print("输入的长度为：",num)
+
+htuple = (1,"字符串",59568,"哈哈哈",true,"哈哈哈哈","kfc")
+print(htuple[4])
